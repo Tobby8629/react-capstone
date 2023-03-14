338a463e851f90176be7bdb86518bc1191f7748d
tobby8629 <sarhmullah15@gmail.com>
2023-03-14 08:31:39 -0700

    clear lint error

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,7 +24,13 @@ function Home() {
         {data.filter((info) => info.date.includes(date)).map((data) => (
           <div className="each" key={data.id}>
             <div className="detail">
-              <h3>Year: <span> {data.date}</span></h3>
+              <h3>
+                Year:
+                <span>
+                  {' '}
+                  {data.date}
+                </span>
+              </h3>
               <p>
                 <span className="total">Total revenue: </span>
                 {data.total}
